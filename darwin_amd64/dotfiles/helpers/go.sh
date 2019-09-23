a96fb307aab79d2bca4788c2207dccc70e4dd553
Matthew Bamber <mattb@grabyo.com>
2019-09-23 08:47:38 +0100

    :up: Add function for obtaining go test coverage

--- darwin_amd64/dotfiles/helpers/go.sh
+++ darwin_amd64/dotfiles/helpers/go.sh
@@ -5,3 +5,15 @@ echo "Ensuring go is properly configured..."
 export GOPATH="$HOME/go"
 export GOBIN="$GOPATH/bin"
 export PATH=$GOPATH:$GOBIN:$PATH
+
+func gocoverage() {
+    rm -rf ./.testcoverage
+    mkdir -p ./.testcoverage
+    for f in $(go list ./...); do
+        go test -covermode=count -coverprofile ./.testcoverage/$(echo "$f" | tr / -).cover "$f"
+    done
+    echo "mode: count" > "./.testcoverage/all.out"
+    grep -h -v "^mode:" ./.testcoverage/*.cover >> "./.testcoverage/all.out"
+
+    go tool cover -html=./.testcoverage/all.out
+}
