92b869a530ff3c703d44ffee4eec5025911279ae
Matthew Bamber <mattb@grabyo.com>
2020-02-03 16:45:07 +0000

    Create gl and gle for git log and git log with emojify

--- darwin_amd64/dotfiles/helpers/git.sh
+++ darwin_amd64/dotfiles/helpers/git.sh
@@ -2,7 +2,8 @@
 
 echo "Adding git aliases..."
 
-alias gl="git log --pretty=oneline --graph --decorate --color | emojify | less -R"
+alias gl="git log --graph --decorate --color --pretty='format: %C(auto) %>(50,trunc)%d %h %<(100,trunc)%s %Cred %<(24)%cd %C(magenta) authored by: %<(20,trunc)%an' | less -R"
+alias gle="git log --graph --decorate --color --pretty=oneline | emojify | less -R"
 gt() {
     git fetch --prune
     gonebranches=$(git branch -v | grep '\[gone\]')
